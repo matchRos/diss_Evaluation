1d6d4a150ec7541cd414e07159ea12a9c5ba7e5b
Tobias Recker <Recker@match.uni-hannover.de>
2023-11-07 17:54:57 +0100

    adjusted size to fit scale

--- dezentralized_controller_tuning/scripts/lissajous_response_pub.py
+++ dezentralized_controller_tuning/scripts/lissajous_response_pub.py
@@ -14,13 +14,13 @@ from copy import deepcopy
 class LissajousResponsePublisher:
     
     def config(self):
-        self.Ax = rospy.get_param("~Ax", 2.0)
-        self.Ay = rospy.get_param("~Ay", 2.0)
+        self.Ax = rospy.get_param("~Ax", 4.0)
+        self.Ay = rospy.get_param("~Ay", 4.0)
         self.omega_x = rospy.get_param("~omega_x", 1.0)
         self.omega_y = rospy.get_param("~omega_y", 2.0)
         self.delta_x = rospy.get_param("~delta_x", 0.0)
         self.delta_y = rospy.get_param("~delta_y", 0) * math.pi/180.0
-        self.velocity = rospy.get_param("~velocity", 0.1)
+        self.velocity = rospy.get_param("~velocity", 0.03)
         self.orientation_offset = rospy.get_param("~orientation_offset", 70) * math.pi/180.0 
         self.number_of_points = rospy.get_param("~number_of_points", 1000)
         self.lissajous_path_topic = rospy.get_param("~lissajous_path_topic", "/lissajous_path")
